f72608c83c893ccbab2d4abff7973a2c5ffbf98f
growlitics <contact@growlitics.ai>
2025-08-06 11:02:32 +0200

    Update sidebar user information

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -103,10 +103,10 @@ const Sidebar = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Gastbedrijf
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
-                  VP Fancy Admin
+                  Gast1
                 </Typography>
               </Box>
             </Box>
